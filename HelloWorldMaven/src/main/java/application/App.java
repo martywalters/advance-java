cf8b28b73a459910dcab8ddff298ef65b0584e68
walters <walters@Ubuntu.myguest.virtualbox.org>
2023-08-04 15:26:56 -0700

    Hello World Maven

---
+++ HelloWorldMaven/src/main/java/application/App.java
@@ -0,0 +1,16 @@
+package application;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        for (int i=0 ; i < 5 ; i++) {
+            System.out.println("line "+ i);
+        }
+    }
+}
